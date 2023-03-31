8839b5ea04a01b913dada1d1416a32cfbcf6129d
NIkki <nikki@Apples-MacBook-Pro.local>
2023-03-31 12:00:45 +0530

    react ep 1

---
+++ app.js
@@ -0,0 +1,32 @@
+// const header = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World From React!!!"
+// );
+
+// console.log(header); //returns object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(header);
+
+// {
+//   /*
+//  <div id='parent'>
+//     <div id='child'>
+//         <h1></h1>
+//     </div>
+// </div>
+//  */
+// }
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "h1 from child element")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
